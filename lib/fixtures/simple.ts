ad0d62cbeec89567a2f18564814d698f399cfaff
Ash Wilson <smashwilson@github.com>
2019-08-27 15:39:28 -0400

    Play with branches a bit

--- lib/fixtures/simple.ts
+++ lib/fixtures/simple.ts
@@ -3,4 +3,11 @@ import {Repository} from "../toolkit/api";
 export default function(repo: Repository) {
   repo.repo("smashwilson/simple");
   repo.times(3, () => repo.commit());
+
+  repo.branch("something", () => {
+    repo.times(2, () => repo.commit());
+  });
+
+  repo.commit({message: "Fixed commit message here"});
+  repo.times(4, () => repo.commit());
 }
